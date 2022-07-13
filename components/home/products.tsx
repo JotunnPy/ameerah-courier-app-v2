974ff18929b0b88d3b86c3f6887bd58f9694eaa4
JotunnPy <106374742+JotunnPy@users.noreply.github.com>
2022-07-12 22:31:44 -0700

    Update products.tsx

--- components/home/products.tsx
+++ components/home/products.tsx
@@ -41,6 +41,7 @@ function Products() {
                 <br />
                 <MediaQuery smallerThan={'md'} styles={{display: 'none'}}>
                 <SimpleGrid cols={4}>
+                    
                     <motion.div key={'elec'} initial={{opacity: 0}} transition={{duration: 1, delay: 0.5}} whileInView={{opacity: 1}}><ArticleCardImage category='Shop' title='Electronics' image='https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80' /></motion.div>
                     <motion.div key={'clot'}  initial={{opacity: 0}} transition={{duration: 1, delay: 1}} whileInView={{opacity: 1}}><ArticleCardImage category='Shop' title='Clothing' image='https://images.unsplash.com/photo-1560769629-975ec94e6a86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80' /></motion.div>
                     <motion.div key={'esse'}  initial={{opacity: 0}} transition={{duration: 1, delay: 1.5}} whileInView={{opacity: 1}}><ArticleCardImage category='Shop' title='Essentials' image='https://images.unsplash.com/photo-1525904097878-94fb15835963?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80' /></motion.div>
